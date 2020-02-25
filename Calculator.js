5dcea511be8f469674a33899f276383f03b13645
lawrencepytleski2 <59966826+lawrencepytleski2@users.noreply.github.com>
2020-02-25 15:22:36 -0500

    Delete Calculator.js

--- Calculator.js
+++
@@ -1,7 +0,0 @@
-let button1 = document.getElementById("Number 7");
-let button2 = document.getElementById("Number 8");
-let button3 = document.getElementById("Number 9");
-
-button1.addEventListener( "click", 7 );
-button2.addEventListener( "click", 8 );
-button3.addEventListener( "click", 9 );
