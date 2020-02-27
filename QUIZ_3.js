501160f0a7a1146ce0df982ccf5f59d5b662bb69
lawrencepytleski2 <59966826+lawrencepytleski2@users.noreply.github.com>
2020-02-27 18:28:00 -0500

    Add files via upload

---
+++ QUIZ_3.js
@@ -0,0 +1,29 @@
+/**
+ * Quiz 3 focuses on Objects
+ *
+ * @category    Quiz 3 Alt
+ * @author      Chad Banks <crbanks1@hfcc.edu>
+ * @author      LAWRENCE PYTLESKI <ljpytleski@hawkmail.hfcc.edu>
+ * @date        2020.02.27
+ * @grade
+ */
+
+let FoodTruck ={}
+
+let FoodTruck =
+    {
+        Name:       FoodNameBox.value,
+        MPG:        FoodMPGBox.value,
+        FoodType:   FoodFoodTypeBox.value,
+        Sales:      FoodSalesBox.value,
+        Stops:      FoodStopsBox.value,
+        Employees:  FoodEmployeesBox.value
+    };
+
+function addStop()
+{
+
+}
+
+FoodTruck.push(addStop);
+console.log(addStop);
