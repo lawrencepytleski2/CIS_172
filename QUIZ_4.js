b9bec2a443739919fd35c35e90a734ca8f9443be
Chad Banks <chadbanks@quickenloans.com>
2020-03-05 18:13:02 -0500

    Graded Q4.

--- QUIZ_4.js
+++ QUIZ_4.js
@@ -5,31 +5,31 @@
  * @author      Chad Banks <crbanks1@hfcc.edu>
  * @author      LAWRENCE PYTLESKI <ljpytleski@hawkmail.hfcc.edu>
  * @date        2020.02.27
- * @grade
+ * @grade       5 / 10
  */
 
 // Use my page for all examples below.
 // https://cislinux.hfcc.edu/~crbanks1
 
-// 2 pts
+// 1.5 / 2 pts
 // 1. Use the JS to get my footer into a variable called foot.
 
-var foot = document.getElementById("myFooter").name;
+var foot = document.getElementById("myFooter");
 
-// 3 pts
+// 1 / 3 pts
 // 2. Add an event to my footer to fire a method when clicked. Any method name is fine.
 
 <button onclick="document.getElementById('demo').innerHTML = Date()">What time is it?</button>
+foot.addEventListener('click', someMethodName);
 
-// 2 pts
+// 1 / 2 pts
 // 3. Store my title element in a variable called myTitle.
 
-var myTitle = "CIS-172-70 20W Dashboard";
+var myTitle = document.getElementById("myTitle");
 
-// 3 pts
+// 1.5 / 3 pts
 // 4. Use JS to change my Title and Footer to say "This Class rocks!"
 
-<title id="demo"></title>
-<footer id='demo'></footer>
-var myTitle = "This Class rocks!";
-document.getElementById("demo").innerHTML = myTitle;
+
+document.getElementById("myFooter").innerHTML = "This Class rocks!";
+document.getElementById("myTitle").innerHTML = "This Class rocks!";
