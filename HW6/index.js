daa598b39eb9be08d0f4ac9e3d57c2b4647fddf5
Chad Banks <chadrbanks@yahoo.com>
2020-05-05 16:31:44 -0400

    HW 6

--- HW6/index.js
+++ HW6/index.js
@@ -5,70 +5,56 @@
  * @author      Lawrence Pytleski
  * @date
  * @link     ljpytleski@hawkmail.hfcc.edu
-    * @grade
+ * @grade       10 / 10
  */
 
 let PageHandler = {
+  pageDiv: null,
+  isHidden: true,
 
-    pageDiv : null,
-    isHidden : true,
+  init: function () {
+    try {
+      console.log("Page has loaded!");
 
-    init: function () {
-        try {
-        console.log("Page has loaded!");
+      PageHandler.pageDiv = $("#JohnCena");
+      PageHandler.pageDiv.hide();
 
-        PageHandler.pageDiv = $('#JohnCena');
-        PageHandler.pageDiv.hide();
+      let showDiv = $("#showDiv");
+      showDiv.click(PageHandler.onReturnButtonClick);
 
-        let showDiv = $('#showDiv');
-        showDiv.click(PageHandler.onReturnButtonClick);
-
-        $('#jQueryButton').click( PageHandler.onReturnButtonClick );
-        }
-
-    catch(err) {
-        console.error("Error!");
-        throw "Error!";
-        }
-    },
-
-    onReturnButtonClick : function( event )
-    {
-        try {
-
-        if(PageHandler.isHidden)
-        {
-            PageHandler.pageDiv.show();
-            PageHandler.isHidden = false;
-        }
-        else
-        {
-            PageHandler.pageDiv.hide();
-            PageHandler.isHidden = true;
-        }
-        }
-        catch(err) {
-            console.error("Error!");
-            throw "Error!";
-        }
-    },
-
-    onReturnButtonClick : function( event )
-    {
-        try {
-            $('a[href=http://www.google.com]').click(function () {
-                window.open(this.href);
-                return false;
-            });
-        }
-        catch(err) {
-            console.error("Error!");
-            throw "Error!";
-        }
+      $("#jQueryButton").click(PageHandler.onReturnButtonClick);
+    } catch (err) {
+      console.error("Error!");
+      throw "Error!";
     }
+  },
 
+  onReturnButtonClick: function (event) {
+    try {
+      if (PageHandler.isHidden) {
+        PageHandler.pageDiv.show();
+        PageHandler.isHidden = false;
+      } else {
+        PageHandler.pageDiv.hide();
+        PageHandler.isHidden = true;
+      }
+    } catch (err) {
+      console.error("Error!");
+      throw "Error!";
+    }
+  },
 
+  onReturnButtonClick: function (event) {
+    try {
+      $("a[href=http://www.google.com]").click(function () {
+        window.open(this.href);
+        return false;
+      });
+    } catch (err) {
+      console.error("Error!");
+      throw "Error!";
+    }
+  },
 };
-
 
 window.onload = PageHandler.init;
